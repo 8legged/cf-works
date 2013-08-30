bebb031cce10cdb00620fc13964aaac1ed838d5a
Matt Smith <matt@nearapogee.com>
2013-08-29 20:35:40 -0700

    add homes

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,4 @@
 class Work < ActiveRecord::Base
   has_many :characters
+  has_many :homes, through: :characters
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130829234818) do
+ActiveRecord::Schema.define(version: 20130830031844) do
 
   create_table "characters", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "work_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "homes", force: true do |t|
+    t.string   "location"
+    t.integer  "character_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
